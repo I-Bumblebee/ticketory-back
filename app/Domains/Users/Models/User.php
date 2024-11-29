a250f49da6423e8fb0b5ec4c91587b3ec701ae7c
luka trapaidze <trapaidze.luka@kiu.edu.ge>
2024-11-30 02:12:27 +0400

    feat: add user factory and api resource

--- app/Domains/Users/Models/User.php
+++ app/Domains/Users/Models/User.php
@@ -2,9 +2,9 @@
 
 namespace App\Domains\Users\Models;
 
-use App\Domains\Users\Notifications\VerifyEmail;
 use Database\Factories\UserFactory;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -14,7 +14,6 @@ use Illuminate\Notifications\Notifiable;
  */
 class User extends Authenticatable implements MustVerifyEmail
 {
-    /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
 
     protected $fillable = [
@@ -37,8 +36,15 @@ class User extends Authenticatable implements MustVerifyEmail
         ];
     }
 
-    public function sendEmailVerificationNotification(): void
+    protected static function newFactory(): UserFactory
     {
-        $this->notify(new VerifyEmail);
+        return new UserFactory;
+    }
+
+    protected function fullName(): Attribute
+    {
+        return Attribute::make(
+            get: fn () => "$this->name $this->lastname"
+        );
     }
 }
---
+++ app/Domains/Users/Resources/UserResource.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Domains\Users\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'lastname' => $this->lastname,
+            'full_name' => $this->full_name,
+            'email' => $this->email,
+        ];
+    }
+}
