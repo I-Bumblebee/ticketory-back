0a8a98ef45b8d8dab4fee9cd666f02ba92e66392
luka trapaidze <trapaidze.luka@kiu.edu.ge>
2024-11-30 02:15:18 +0400

    wip: added user auth endpoints

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,12 @@
 <?php
 
+use App\Domains\Users\Actions\GetAuthenticatedUserAction;
+use App\Domains\Users\Actions\LoginUserAction;
+use App\Domains\Users\Actions\RegisterUserAction;
 use App\Domains\Users\Actions\VerifyUserEmailAction;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
 Route::get('/verify/{user}/{hash}', VerifyUserEmailAction::class)->middleware('signed:relative')->name('verification.verify');
+Route::post('/register', RegisterUserAction::class);
+Route::post('/login', LoginUserAction::class);
+Route::get('/user', GetAuthenticatedUserAction::class)->middleware('auth:sanctum');
